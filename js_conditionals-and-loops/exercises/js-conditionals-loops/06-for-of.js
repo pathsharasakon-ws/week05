eb0c742c35185149f79e18e4a7165eca0546c596
Pathsharasakon <pathsharasakon@gmail.com>
2026-07-31 20:38:41 +0700

    exercise06

--- js_conditionals-and-loops/exercises/js-conditionals-loops/06-for-of.js
+++ js_conditionals-and-loops/exercises/js-conditionals-loops/06-for-of.js
@@ -8,7 +8,15 @@
 // array or string of vowels plus .includes(char) makes the check easy.
 
 function countVowels(word) {
-  // TODO: replace this with your implementation
+  const vowels = "aeiou";
+  let count = 0;
+
+  for (const char of word) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
 }
 
 // --- Tests (do not edit) ---
