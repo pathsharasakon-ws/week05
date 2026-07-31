f8fc9833b4f767f27d74360a643b049aee79be43
Pathsharasakon <pathsharasakon@gmail.com>
2026-07-31 14:02:39 +0700

    condition 03

--- js_conditionals-and-loops/exercises/js-conditionals-loops/03-ternary-and-guard.js
+++ js_conditionals-and-loops/exercises/js-conditionals-loops/03-ternary-and-guard.js
@@ -11,7 +11,11 @@
 //      a / b. Express this decision as a single ternary expression.
 
 function safeDivide(a, b) {
-  // TODO: replace this with your implementation
+  if(typeof a !== 'number' || typeof b !== 'number') {
+    return "invalid input";
+  }
+
+  return b === 0 ? "cannot divide by zero" : a / b;
 }
 
 // --- Tests (do not edit) ---
